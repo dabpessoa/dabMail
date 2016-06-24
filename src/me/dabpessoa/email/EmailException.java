ea5fa2a36597986a3348d3c13c58b60e15e1266f
Diego Pessoa <dabpessoa@gmail.com>
2016-06-24 11:55:14 -0300

    initial commit.

---
+++ src/me/dabpessoa/email/EmailException.java
@@ -0,0 +1,20 @@
+package me.dabpessoa.email;
+
+public class EmailException extends Exception {
+	private static final long serialVersionUID = 1L;
+
+	public EmailException() {}
+	
+	public EmailException(String message) {
+		super(message);
+	}
+	
+	public EmailException(Throwable throwable) {
+		super(throwable);
+	}
+	
+	public EmailException(String message, Throwable throwable) {
+		super(message, throwable);
+	}
+	
+}
---
+++ src/me/dabpessoa/test/Main.java
@@ -0,0 +1,35 @@
+package me.dabpessoa.test;
+
+import java.io.IOException;
+import java.util.Arrays;
+
+import me.dabpessoa.email.Email;
+import me.dabpessoa.email.EmailException;
+import me.dabpessoa.email.EmailSender;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		EmailSender sender = new EmailSender("smtp.seduc.ce.gov.br", "financeiro@seduc.ce.gov.br", "f!an@489");
+		sender.setProtocol("smtps");
+		
+		Email email = new Email();
+		email.setFrom("diego.pessoa@seduc.ce.gov.br");
+		email.setTos(Arrays.asList("diego.pessoa@seduc.ce.gov.br"));
+		email.setSubject("teste de envio de email <título>");
+		email.setMessage("teste de corpo de e-mail");
+		
+		try {
+			sender.doSend(email);
+		} catch (EmailException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+	}
+	
+}
